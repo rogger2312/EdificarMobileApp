7288622c620052d4ca505ab22451d3413364bf67
rogger2312 <rogger2312@gmail.com>
2020-06-17 10:55:36 -0500

    Agrega clases a model

---
+++ app/src/main/java/com/example/edifikarmobileapp/model/Property.kt
@@ -0,0 +1,8 @@
+package com.example.edifikarmobileapp.model
+
+class Property (
+    val photo :String,
+    val logo : String,
+    val name : String,
+    val ubicacion : String
+)
---
+++ app/src/main/java/com/example/edifikarmobileapp/model/Service.kt
@@ -0,0 +1,9 @@
+package com.example.edifikarmobileapp.model
+
+class Service (
+    val name : String,
+    val direccion : String,
+    val company : Boolean,
+    val icono : String,
+    val horario : String
+)
